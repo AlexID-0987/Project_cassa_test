5e5264ac75ee2896bf914ad70018482558f03d57
Olexiy <a1980dominov@gmail.com>
2022-02-05 23:30:20 +0200

    Create Crud metods,

--- Project_cassa_test/Controllers/Api/PurchaseController.cs
+++ Project_cassa_test/Controllers/Api/PurchaseController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Project_cassa_test.Controllers.Api
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/[controller]")]
     public class PurchaseController : Controller
     {
         private readonly PurchaseContext context;
@@ -19,6 +19,16 @@ namespace Project_cassa_test.Controllers.Api
         public async Task<ActionResult<IEnumerable<Purchase>>> Get()
         {
             return await context.Purchases.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task <ActionResult<Purchase>> Get(int id)
+        {
+            Purchase purchase = await context.Purchases.FirstOrDefaultAsync(x => x.Id == id);
+            if(purchase == null)
+            {
+                return NotFound();
+            }
+            return purchase;
         }
     }
 }
